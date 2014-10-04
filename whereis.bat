85aeb8e72c3b1d77393233457cad7829a1c083f7
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2014-10-04 01:29:20 -0500

    Update whereis.bat

--- whereis.bat
+++ whereis.bat
@@ -1,1 +1,4 @@
+::
+:: Alternatively, use `where` in `cmd`, or in PowerShell `where.exe` or `Get-Command` (`gcm`).
+::
 @for %%e in (%PATHEXT%) do @for %%i in (%1%%e) do @if NOT "%%~$PATH:i"=="" echo %%~$PATH:i
